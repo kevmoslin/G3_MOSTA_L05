05a6305b953051d7350ea83fed8400ff1f0a38ae
kevmoslin <kevinmostacerolinares@gmail.com>
2025-05-06 12:32:16 -0600

    error

--- Ejercicios/main.java
+++ Ejercicios/main.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class main {
     public static void Main(String[] args) {
-        
+        Node<Integer> lista1 = null;
+
+        lista1 = Metodos.agregarTarea(lista1, 1);
+        lista1 = Metodos.agregarTarea(lista1, 2);
+        lista1 = Metodos.agregarTarea(lista1, 3);
     }
 }
