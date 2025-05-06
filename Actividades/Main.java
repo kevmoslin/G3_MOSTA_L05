7b8c004c25d24d57185000ebaaf70291e0e1987c
kevmoslin <kevinmostacerolinares@gmail.com>
2025-05-06 02:06:41 -0600

    agregando tareas

--- Actividades/Main.java
+++ Actividades/Main.java
@@ -5,5 +5,8 @@ public class Main {
         GestordeTareas<Tarea> gestor = new GestordeTareas<>();
 
         gestor.agregarTarea(new Tarea("generar informe", 2));
+        gestor.agregarTarea(new Tarea("envia notificacion", 5));
+        gestor.agregarTarea(new Tarea("pedido", 1));
+        gestor.agregarTarea(new Tarea("revisar", 3));
     }
 }
