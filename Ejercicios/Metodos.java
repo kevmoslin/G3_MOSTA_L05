39161fc96d17bfcbc53afb1b8877a9e286d6bd5e
kevmoslin <kevinmostacerolinares@gmail.com>
2025-05-06 11:44:11 -0600

    metodos q pide

--- Ejercicios/Metodos.java
+++ Ejercicios/Metodos.java
@@ -1,4 +1,34 @@
 package Ejercicios;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Metodos {
-    
+    public static <T> boolean buscarElemento(List<T> lista, T valor){
+
+    }
+
+    public static <T> List<T> invertirLista(List<T> lista){
+
+    }
+
+    public static <T> void insertarAlFinal(Node<T> head, T valor) {
+
+    }
+
+    public static <T> int contarNodos(Node<T> head){
+
+    }
+
+    public static <T> boolean sonIguales(Node<T> head1, Node<T> head2){
+
+    }
+
+    public static <T> Node<T> concatenarListas(Node<T> head1, Node<T> head2){
+
+    }
+
+    public static <T> void imprimirLista(Node<T> head){
+        
+    }
 }
