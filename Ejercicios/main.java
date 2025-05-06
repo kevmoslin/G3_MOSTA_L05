34b7acd934fce1e3e5457a092e9690c2ac2550f1
kevmoslin <kevinmostacerolinares@gmail.com>
2025-05-06 12:33:24 -0600

    error por nombre de metodo incorrecto

--- Ejercicios/main.java
+++ Ejercicios/main.java
@@ -7,8 +7,8 @@ public class main {
     public static void Main(String[] args) {
         Node<Integer> lista1 = null;
 
-        lista1 = Metodos.agregarTarea(lista1, 1);
-        lista1 = Metodos.agregarTarea(lista1, 2);
-        lista1 = Metodos.agregarTarea(lista1, 3);
+        lista1 = Metodos.insertarAlFinal(lista1, 1);
+        lista1 = Metodos.insertarAlFinal(lista1, 2);
+        lista1 = Metodos.insertarAlFinal(lista1, 3);
     }
 }
